d678d02425c1787857ccde53884771423e6f684d
IdkWhatToPut179 <iacopo.paola06@gmail.com>
2026-02-15 14:44:11 +0100

    Create dom.js

---
+++ src/ui/dom.js
@@ -0,0 +1,10 @@
+export function getDom() {
+  return {
+    overlay: document.getElementById("overlay"),
+    hint: document.getElementById("hint"),
+    inventoryHotbar: document.getElementById("inventory"),
+    inventoryPanel: document.getElementById("inventoryPanel"),
+    hotbarGrid: document.getElementById("hotbarGrid"),
+    bagGrid: document.getElementById("bagGrid")
+  };
+}
